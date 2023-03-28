7138ed8ae231602ee30c655867695666771c436b
mjy waiting <zhiyangqiu0763@gmail.com>
2023-03-28 11:45:51 +0800

    第一次提交

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<!--
+ * @作者: kerwin
+ * @公众号: 大前端私房菜
+-->
+<template>
+    <div>
+        login
+
+        <button @click="handleLogin">login</button>
+    </div>
+</template>
+
+<script setup>
+
+const handleLogin = ()=>{
+    localStorage.setItem("token","kerwin")
+}
+</script>
+
---
+++ src/views/MainBox.vue
@@ -0,0 +1,12 @@
+<!--
+ * @作者: kerwin
+ * @公众号: 大前端私房菜
+-->
+<template>
+    <div>
+        <div>navbar</div>
+        <div>sidebar</div>
+
+        <router-view></router-view>
+    </div>
+</template>
